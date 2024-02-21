477807231684963d2f6d1091be852ad575e565dc
nimje.p@northeastern.edu <nimje.p@northeastern.edu>
2024-02-21 14:34:28 -0500

    added terraform resources to create webapp instance from custom image, and firewall rules

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "vpc_network" {
   name                            = "${var.environment}-vpc-network"
   auto_create_subnetworks         = false
   delete_default_routes_on_create = true
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.vpc_routing_mode
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
@@ -20,9 +20,62 @@ resource "google_compute_subnetwork" "db_subnet" {
   ip_cidr_range = var.db_subnet_cidr_block
 }
 
-resource "google_compute_route" "custom_route" {
-  name             = "${var.environment}-route"
+resource "google_compute_route" "internet_route" {
+  name             = "${var.environment}-internet-route"
   network          = google_compute_network.vpc_network.id
-  dest_range       = "0.0.0.0/0"
+  dest_range       = var.internet_gateway_cidr
   next_hop_gateway = "default-internet-gateway"
+}
+
+resource "google_compute_firewall" "allow_http_rule" {
+  project  = var.project_id
+  name     = "${google_compute_network.vpc_network.name}-allow-http"
+  network  = google_compute_network.vpc_network.id
+  disabled = var.allow_http_disabled
+
+  allow {
+    protocol = "tcp"
+    ports    = var.allow_http_ports
+  }
+
+  source_ranges = var.allow_http_source_ranges
+  target_tags   = var.allow_http_target_tags
+}
+
+resource "google_compute_firewall" "deny_ssh_rule" {
+  project  = var.project_id
+  name     = "${google_compute_network.vpc_network.name}-deny-ssh"
+  network  = google_compute_network.vpc_network.id
+  disabled = var.deny_ssh_disabled
+
+  deny {
+    protocol = "tcp"
+    ports    = var.deny_ssh_ports
+  }
+
+  source_ranges = var.deny_ssh_source_ranges
+  target_tags   = var.deny_ssh_target_tags
+}
+
+resource "google_compute_instance" "webapp_vm" {
+  name         = "${var.environment}-${var.webapp_instance_name}"
+  machine_type = var.webapp_instance_machine_type
+  zone         = var.webapp_instance_zone
+  tags         = var.webapp_instance_tags
+
+  boot_disk {
+    initialize_params {
+      image = var.webapp_instance_image_name
+      size  = var.webapp_instance_disk_size
+      type  = var.webapp_instance_disk_type
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.webapp_subnet.self_link
+
+    access_config {
+      network_tier = var.webapp_instance_network_tier
+    }
+  }
 }
--- variables.tf
+++ variables.tf
@@ -5,12 +5,22 @@ variable "environment" {
 
 variable "project_id" {
   type        = string
-  description = "Default project id"
+  description = "GCP project id"
 }
 
 variable "region" {
   type        = string
   description = "GCP region"
+}
+
+variable "vpc_routing_mode" {
+  type        = string
+  description = "Routing mode for the vpc network"
+}
+
+variable "internet_gateway_cidr" {
+  type        = string
+  description = "Route destination ip address range for internet gateway"
 }
 
 variable "webapp_subnet_name" {
@@ -32,3 +42,104 @@ variable "db_subnet_cidr_block" {
   type        = string
   description = "Database subnet CIDR block"
 }
+
+variable "allow_http_ports" {
+  type        = list(string)
+  description = "List of allowed ports for incoming http traffic"
+}
+
+variable "allow_http_source_ranges" {
+  type        = list(string)
+  description = "List of source ip ranges for incoming http traffic"
+}
+
+variable "allow_http_target_tags" {
+  type        = list(string)
+  description = "List of network tags of target instance"
+}
+
+variable "allow_http_disabled" {
+  type        = bool
+  description = "Disable allow http firewall rule"
+}
+
+variable "deny_ssh_ports" {
+  type        = list(string)
+  description = "List of denied ports for incoming ssh traffic"
+}
+
+variable "deny_ssh_source_ranges" {
+  type        = list(string)
+  description = "List of source ip ranges for incoming ssh traffic"
+}
+
+variable "deny_ssh_target_tags" {
+  type        = list(string)
+  description = "List of network tags of target instance"
+}
+
+variable "deny_ssh_disabled" {
+  type        = bool
+  description = "Disable deny ssh firewall rule"
+}
+
+variable "allow_egress_ports" {
+  type        = list(string)
+  description = "List of allowed ports for outgoing traffic"
+}
+
+variable "allow_egress_dest_ranges" {
+  type        = list(string)
+  description = "List of destination ip ranges for outgoing traffic"
+}
+
+variable "allow_egress_target_tags" {
+  type        = list(string)
+  description = "List of network tags of target instance"
+}
+
+variable "allow_egress_disabled" {
+  type        = bool
+  description = "Disable allow egress firewall rule"
+}
+
+variable "webapp_instance_name" {
+  type        = string
+  description = "Name for the webapp instance"
+}
+
+variable "webapp_instance_machine_type" {
+  type        = string
+  description = "Preset GCE machine type for the webapp instance"
+}
+
+variable "webapp_instance_zone" {
+  type        = string
+  description = "Zone where the webapp instance should be created"
+}
+
+variable "webapp_instance_tags" {
+  type        = list(string)
+  description = "List of network tags to attach to the webapp instance"
+}
+
+variable "webapp_instance_disk_type" {
+  type        = string
+  description = "Disk type to attach to the webapp instance"
+}
+
+variable "webapp_instance_image_name" {
+  type        = string
+  description = "Image name or image family name to initialize boot disk for the webapp instance"
+}
+
+variable "webapp_instance_disk_size" {
+  type        = string
+  description = "Disk size to attach to the webapp instance"
+}
+
+variable "webapp_instance_network_tier" {
+  type        = string
+  description = "Network tier of the webapp instance"
+}
+
